28f28ffa2658d146b9c9b16685f20679eb98d14b
Eduardo Alba <eduardo.alba@gmail.com>
2020-04-05 21:42:29 -0300

    add files

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="jumbotron">
+      <h1>Site Administration</h1>
+      <p>This is where you can configure your web application</p>
+      <a href="/about">About</a>
+    </div>
+  );
+}
+
+export default HomePage;
