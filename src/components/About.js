ca1464e7240ed969c7e883a73329defa7e90e77d
Eduardo Alba <eduardo.alba@gmail.com>
2020-04-07 21:56:51 -0300

    add courses page

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ class AboutPage extends React.Component {
   render() {
     return (
       <>
-        <h1>About</h1>
+        <h2>About</h2>
         <p>This app uses React.</p>
       </>
     );
