a9468ef4866f339d4585cc9a2dd5b5eaf237771b
Eduardo Alba <eduardo.alba@gmail.com>
2020-04-10 19:49:50 -0300

    implement router

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
     <div className="jumbotron">
       <h1>Site Administration</h1>
       <p>This is where you can configure your web application</p>
-      <a href="/about">About</a>
+      <Link to="about" className="btn btn-primary">
+        About
+      </Link>
     </div>
   );
 }
