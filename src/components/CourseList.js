df39a750ff3784393869c0f350e4000690520954
Eduardo Alba <eduardo.alba@gmail.com>
2020-04-11 21:20:57 -0300

    add texinput componet and forms

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { PropTypes } from "prop-types";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 function CourseList(props) {
@@ -8,7 +8,7 @@ function CourseList(props) {
       <thead>
         <tr>
           <th>Title</th>
-          <th>Author Id</th>
+          <th>Author ID</th>
           <th>Category</th>
         </tr>
       </thead>
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 function HomePage() {
   return (
     <div className="jumbotron">
-      <h1>Site Administration</h1>
-      <p>This is where you can configure your web application</p>
+      <h1>Pluralsight Administration</h1>
+      <p>React, Flux, and React Router for ultra-responsive web apps.</p>
       <Link to="about" className="btn btn-primary">
         About
       </Link>
